d441c9776a4b32514b8eba23ee40ccf2b28a4d69
Chris Nolan.ca <chrisnolan.ca+github@gmail.com>
2008-09-26 22:04:56 -0400

    Pearson Correlation function

--- recommendations.rb
+++ recommendations.rb
@@ -54,3 +54,55 @@ def simularity_distance(preferences, person1, person2)
 end
 
 puts simularity_distance(@critics, 'Lisa Rose', 'Gene Seymour')
+
+# Pearson Correlation from page 13
+def simularity_pearson(preferences, person1, person2)
+  # Get the list of shared_items
+  shared_items = {}
+  for item in preferences[person1].keys
+    shared_items[item] = 1 if preferences[person2].keys.include? item
+  end
+  
+  # nothing in common
+  return 0 if shared_items.empty?
+  
+  # Add up all the preferences
+  sum1 = shared_items.keys.inject(0) {|sum, item| sum + preferences[person1][item]}
+  sum2 = shared_items.keys.inject(0) {|sum, item| sum + preferences[person2][item]}
+  
+  # Sum up the squares
+  sum1_squared = shared_items.keys.inject(0) {|sum, item| sum + preferences[person1][item]**2}
+  sum2_squared = shared_items.keys.inject(0) {|sum, item| sum + preferences[person2][item]**2}
+  
+  # Sum up the productions
+  product_sum = shared_items.keys.inject(0) {|sum, item| sum + preferences[person1][item] * preferences[person2][item]}
+  
+  # Calculate Pearson score
+  num = product_sum - (sum1*sum2/shared_items.length)
+  den = Math.sqrt((sum1_squared - sum1**2/shared_items.length) * (sum2_squared - sum2**2/shared_items.length))
+  den.zero? ? 0 : num/den
+end
+
+puts simularity_pearson(@critics, 'Lisa Rose', 'Gene Seymour')
+
+# # pseudocode for Pearson Correlation from wikipedia
+# uses a bit easier to follow variable names than the above example
+# http://en.wikipedia.org/wiki/Correlation#Computing_correlation_accurately_in_a_single_pass
+#  sum_sq_x = 0
+#  sum_sq_y = 0
+#  sum_coproduct = 0
+#  mean_x = x[1]
+#  mean_y = y[1]
+#  for i in 2 to N:
+#      sweep = (i - 1.0) / i
+#      delta_x = x[i] - mean_x
+#      delta_y = y[i] - mean_y
+#      sum_sq_x += delta_x * delta_x * sweep
+#      sum_sq_y += delta_y * delta_y * sweep
+#      sum_coproduct += delta_x * delta_y * sweep
+#      mean_x += delta_x / i
+#      mean_y += delta_y / i 
+#  pop_sd_x = sqrt( sum_sq_x / N )
+#  pop_sd_y = sqrt( sum_sq_y / N )
+#  cov_x_y = sum_coproduct / N
+#  correlation = cov_x_y / (pop_sd_x * pop_sd_y)
